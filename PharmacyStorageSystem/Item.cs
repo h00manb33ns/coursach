b182d6160530c9a426e127187b50c134aed5c827
h00manb33ns <tdanik12345@gmail.com>
2021-05-17 15:53:29 +0300

    Menu

---
+++ PharmacyStorageSystem/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PharmacyStorageSystem
+{
+    public class Item
+    {
+        public string name;
+        public float price;
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Наименование: {name} \n Цена: {price}");
+        }
+    }
+}
---
+++ PharmacyStorageSystem/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace PharmacyStorageSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuO();
+
+        }
+
+        public static void MenuO()
+        {
+            Console.WriteLine("Выберите пункт меню:\n" +
+                              "1. Добавить товар на склад\n" +
+                              "2. Посмотреть товары на складе\n" +
+                              "3. Удалить товар со склада\n" +
+                              "4. Выход");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                default:
+                    MenuO();
+                    break;
+            }
+        }
+    }
+}
